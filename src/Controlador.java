620aaade4bb8b170429dfcde3b9d0b11bc19a485
Ale Gómez <gom20347@uvg.edu.gt>
2021-05-28 23:46:57 -0600

    Se define el lector de archivos

--- src/Controlador.java
+++ src/Controlador.java
@@ -1,3 +1,35 @@
+/*******************************************************
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de Datos
+* Profesor: Moises Gonzales
+* Controlador.java
+* Autor: Alejandro Gomez
+* Fecha de creacion: 28-05-21
+* Ultima edicion: 28-05-21
+********************************************************/
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class Controlador {
-    
+
+    //Se define el scanner para leer el archivo de texto
+    Scanner scan = new Scan(System.in);
+
+    //Se define el arraylist a utilizar
+    ArrayList<ArrayList<String>> ArrayObjetos = new ArrayList<>();
+
+    //Ciclo TryCatch para leer el archivo
+    try{
+
+        //Se define el file a utilizar, en esta caso el de guategrafo
+        File GuateGrafo = new File("guategrafo.txt");
+
+    } catch(FileNotFoundException e){ 
+        System.out.println("Archivo no encontrado");}
+
+
 }
---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/*******************************************************
+* Universidad del Valle de Guatemala
+* Algoritmos y Estructuras de Datos
+* Profesor: Moises Gonzales
+* Main.java
+* Autor: Alejandro Gomez
+* Fecha de creacion: 28-05-21
+* Ultima edicion: 28-05-21
+********************************************************/
+
+public class Main {
+
+    public static void main(String[] args) {
+       Controlador C = new Controlador();
+       Vista V = new Vista();
+
+       C();
+       
+
+        scanner.close();
+    }
+}
