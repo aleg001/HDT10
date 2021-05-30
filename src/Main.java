130c65b5ae97d7dde5e11fe7879cc62e15136360
Ale Gómez <gom20347@uvg.edu.gt>
2021-05-30 15:25:03 -0600

    Cambios en el main

--- src/Main.java
+++ src/Main.java
@@ -5,18 +5,14 @@
 * Main.java
 * Autor: Alejandro Gomez
 * Fecha de creacion: 28-05-21
-* Ultima edicion: 28-05-21
+* Ultima edicion: 30-05-21
 ********************************************************/
 
 public class Main {
 
     public static void main(String[] args) {
        Controlador C = new Controlador();
-       Vista V = new Vista();
+       Vista V = new Vista();       
 
-       C();
-       
-
-        scanner.close();
     }
 }
